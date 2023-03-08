c197dec1a76a4fbd7f546887104ca57e7ab89afc
mizunosuke <m.h.0656.longjump@icloud.com>
2023-03-08 14:56:20 +0900

    first commit

---
+++ database/migrations/2023_03_08_054426_create_rankings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rankings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('num_of_participant');
+            $table->foreignId('fish_id')->constrained('fish')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rankings');
+    }
+};
---
+++ database/migrations/2023_03_08_054501_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('attachment');
+            $table->foreignId('fish_id')->constrained('fish')->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('size');
+            $table->date('day_of_fishing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
